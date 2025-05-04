f6af99b84721fabf01ffea2f1cbc5c8c6331742c
Manoj kumar <manoj166174@gmail.com>
2025-05-04 21:14:25 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of subjects: ");
+        int n = sc.nextInt();
+
+        int[] marks = new int[n];
+        int total = 0;
+
+        // Input marks
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = sc.nextInt();
+            total += marks[i];
+        }
+
+        double average = (double) total / n;
+
+        // Display results
+        System.out.println("Total Marks: " + total);
+        System.out.println("Average: " + average);
+
+        // Grade based on average
+        if (average >= 90) {
+            System.out.println("Grade: A+");
+        } else if (average >= 80) {
+            System.out.println("Grade: A");
+        } else if (average >= 70) {
+            System.out.println("Grade: B");
+        } else if (average >= 60) {
+            System.out.println("Grade: C");
+        } else if (average >= 50) {
+            System.out.println("Grade: D");
+        } else {
+            System.out.println("Grade: F (Fail)");
+        }
+
+        sc.close();
+    }
+}
